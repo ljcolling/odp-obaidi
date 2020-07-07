0740430de2eda55e8d0b62f03ef5814ea2ea50b4
Lincoln <lincoln@colling.net.nz>
2020-07-07 22:17:19 +0100

    pushed dockerfile

--- code/download.R
+++
@@ -1,7 +0,0 @@
-# Install ODP helper
-remotes::install_github("ljcolling/ODPHelper", ref = "dev")
-xfun::pkg_attach2("osfr")
-
-obaidi_download_info <- ODPHelper::download_obaidi()
-dir.create("made")
-saveRDS(object = obaidi_download_info, file = "made/obaidi_download_info.Rdata")
